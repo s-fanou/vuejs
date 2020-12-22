68c68ebcc66d347a6e41c0df4dce47679e8699d3
s-fanou <sotiris.fanou@gmail.com>
2020-12-22 11:32:15 +0200

    adds the message component with props

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
+/* It seems that props are used for Vue components for which the data is not passed 
+using the Vue data object in JS but rather the data is assigned directly in the HTML file 
+like this: <message title="Title of my message" body="Body of my message."></message> */
+Vue.component("message", {
+  props: ["title", "body"],
+  //the instructor uses a css library to make the message template
+  //beautiful I just use a <p></p> for demo purposes
+  // v-show="false" hides the element, v-show="true" shows the element
+  template: `<div v-show="isVisible">
+                  <hr>
+                  <p>{{ title }}</p>
+                  <p>{{ body }}</p>
+                  <button type="button" @click="isVisible = false">HIDE</button>
+                  <hr>
+              </div>`,
+
+  data: function () {
+    return { isVisible: true };
+  },
+});
+
 Vue.component("task-item", {
-    //this component is included in the task-list component below
-  template: "<li></li>"
+  //this component is included in the task-list component below
+  template: "<li></li>",
 });
 
 Vue.component("task-list", {
-    //the template must have only one root element therefore we enclose it in a <div></div>
-    template: "<div><task-item v-for='oneTask in mytasks' v-text='oneTask.task'></task-item></div>",
-  
-    // for components the data must be a function which returns a JS object
-    //as compared to the data of the Vue object below which is a simple JS object
-    data: function () {
-      return { mytasks: [
-          {task: 'go to bank', complete: true},
-          {task: 'go to supermarket', complete: false},
-          {task: 'go to pharmacy', complete: false}
-      ] };
-    },
-  });
+  //the template must have only one root element therefore we enclose it in a <div></div>
+  template:
+    "<div><task-item v-for='oneTask in mytasks' v-text='oneTask.task'></task-item></div>",
+
+  // for components the data must be a function which returns a JS object
+  //as compared to the data of the Vue object below which is a simple JS object
+  data: function () {
+    return {
+      mytasks: [
+        { task: "go to bank", complete: true },
+        { task: "go to supermarket", complete: false },
+        { task: "go to pharmacy", complete: false },
+      ],
+    };
+  },
+});
 
 var vueObj = new Vue({
   el: "#app",
