31f140d062c37ce34fe69a47bbf128f9a4c0aa0a
s-fanou <sotiris.fanou@gmail.com>
2020-12-24 12:23:06 +0200

    adds computed properties

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var vueObj = new Vue({
     el: '#app',
-    data: {
-        msgVar: 'Sotiris Fanou',
-        colorVar: 'Red',
+    data: {//the msgVar and colorVar are computed but actually the proper way is below
+        msgVar: 'Sotiris Fanou ' + new Date(),
+        colorVar: 'the color var is ' + 'Red',
         colorsArr: ['red','green','blue'],
         styleVar: 'red-class',
         changeStyleVar: false
     }, //ends data object
+
+    computed: { //these are the proper computed properties
+        computedVar: () => {
+            return "computed var " + new Date;
+        },
+        computedProperty(){
+            //let surVar = "fanou";
+            //surVar1 = surVar.toUpperCase();
+            return "sotiris " + "fanou".toUpperCase();
+        },
+        computedP2: function(){
+            return "another computed P2 " + "shows 3 different ways to write functions"; 
+        }
+
+    }, // ends computed 
+
     methods: {
 
         myFunction(){
@@ -17,8 +33,8 @@ var vueObj = new Vue({
 
         }//ends myFunction()
 
-    }//ends methods object
+    }//ends methods
 });
 
-vueObj.msgVar = "New sotiris message";
+//vueObj.msgVar = "New sotiris message";
 
