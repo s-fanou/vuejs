6cb628278ec90e3d89689446e8772e662d5149ad
s-fanou <sotiris.fanou@gmail.com>
2020-12-28 17:24:16 +0200

    add comments and file after compile

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+/* For Webpack this main.js is the entry point of our application
+  
+we import the Vue library */
 import Vue from 'vue'
+/* I believe we import the whole App.vue file */
 import App from './App.vue'
 
 new Vue({
