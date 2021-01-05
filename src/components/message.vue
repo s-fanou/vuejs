3a8e452690a75b519051fc5b45ef7c6798eeb637
s-fanou <sotiris.fanou@gmail.com>
2021-01-05 15:52:30 +0200

    additional code

---
+++ src/components/message.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'message',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  mounted() {},
+  beforeDestroy() {},
+  methods: {}
+}
+</script>
+
+<style></style>
